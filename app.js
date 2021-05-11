9c50c7a96702bab714dfeec5bd14db6283c4598a
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-05-10 21:18:19 -0400

    Node callbacks and Error Handling

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 //pulls in functions from rectangle.js to use in this file - require is part of node, not vanilla JS
-const rect = require('./rectangle');
+const rect = require('./rectangle'); //rect now refers to a function, not an object
 
 function solveRect(l, w) {
   console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-  if (l <=0 || w<=0) {
-    console.log(`Rectangle dimensions must be greater than or equal to zero. Received: ${l}, ${w}`);
-  } else {
-    console.log(`Area of rectangle: ${rect.area(l,w)}`);
-    console.log(`Permieter of rectangle: ${rect.perimeter(l,w)}`);
-  }
+  //callback function definition
+  rect(l, w, (err, rectangle) => {
+    //checking to see if there is an error
+    if (err) {
+      console.log('ERROR: ', err.message);
+    } else {
+      console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`); //object name is now rectangle as defined in parameter list (also removed parameters since in other function)
+      console.log(`Permieter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+    }
+  });
+  //to see what the order is the the code is being executed - will execute this before showing solutions - solutions will show at the end after the timeouts but still allow other code to run while loading
+  console.log('This statement is logged after the call to rect()');
 }
 
 solveRect(2, 4);
